157cf7b31721a199541fd1ccfb599e57768e22fe
NotHero111 <84732660+NotHero111@users.noreply.github.com>
2021-06-06 22:50:58 +0300

    Добавьте файлы проекта.

---
+++ Operation/Operation.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cout << "task1" << std::endl;
+
+	int a{ 14 };
+	int b{ a - 10 };
+	int c { b * 4 };
+	int d  { c - b + 2 };
+	float x;
+	x = static_cast <float>  (a) * (b + (c / d));
+
+	std::cout << a << std::endl << b << std::endl;
+
+	std::cout << c << std::endl << d << std::endl;
+
+	std::cout << x << std::endl;
+
+
+
+	int k, temp;
+
+	std::string myText;
+
+	const short int NUM21 = 21;
+
+	std::cout << "task2" << std::endl;
+
+	std::cout << "Enter your number = : ";
+	std::cin >> k;
+
+	myText = "check number = ";
+
+	k >= 21 ? temp = (k - NUM21) * 2 : temp = (k - NUM21);
+
+	std::cout << myText << temp << std::endl;
+
+	return 0;
+}
